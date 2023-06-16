71057ba1d03da24a88fc78a3ce6a5fac653bae8b
Wenderson-Oscar <wendersonoscardev@gmail.com>
2023-06-16 01:21:08 -0300

    add like response_comment

--- chat/views.py
+++ chat/views.py
@@ -12,16 +12,17 @@ class ResponseComment(LoginRequiredMixin, CreateView):
     fields = ['comment']
 
     def post(self, request, pk, pk1):
-        get_comment = get_object_or_404(Chat, pk=pk1)
-        filter = self.model.objects.filter(id_book=pk, author=get_comment.author, comment=get_comment.comment)
-        if filter:
-            if request.POST.get('response_msg'):
-                response = filter.update(id_response=request.user.pk, response=request.POST.get('response_msg'))
-                success_url = '/detail_book/' + str(pk)
-                return redirect(success_url)
-            else:
-                return self.form_invalid(self.response)
-
+        parent_comment = get_object_or_404(Chat, pk=pk1)
+        if request.POST.get('response_msg'):
+            response = self.model.objects.create(
+                id_book=pk,
+                comment=request.POST.get('response_msg'),
+                author=request.user,
+                parent_comment=parent_comment
+            )
+            success_url = '/detail_book/' + str(pk)
+            return redirect(success_url)
+    
 
 class CreateComment(LoginRequiredMixin, CreateView):
 
@@ -42,13 +43,13 @@ class AddLikeComment(LoginRequiredMixin, View):
 
     def get(self, request, pk1, pk):
         like_comment = get_object_or_404(Chat, pk=pk1)
-        if Like.objects.filter(like=pk1, author=request.user.id).count() == 0:
-            like = Like(like=pk1, author=request.user.id)
+        if Like.objects.filter(like=pk1, author=self.request.user.id).count() == 0:
+            like = Like(like=pk1, author=self.request.user.id)
             like.save()
             like_comment.quant_like += 1
             like_comment.save()
-        elif Like.objects.filter(like=pk1, author=request.user.id).count() == 1:
-            like = Like.objects.filter(like=pk1, author=request.user.id)
+        elif Like.objects.filter(like=pk1, author=self.request.user.id).count() == 1:
+            like = Like.objects.filter(like=pk1, author=self.request.user.id)
             like.delete()
             like_comment.quant_like -= 1
             like_comment.save()
